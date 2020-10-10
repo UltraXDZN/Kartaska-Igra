74c8c37d01dccedee58b5f3206e965b787883825
UltraX <ultraxdn@gmail.com>
2020-10-10 14:09:12 +0200

    Initial commit

---
+++ KartaskaIgra/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KartaskaIgra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float brojac = 0;
+            while (brojac < 31)
+            {
+                Console.WriteLine("Unesite vrijednost karte: ");
+                float nova_karta = Convert.ToInt32(Console.ReadLine());
+                brojac += nova_karta != 0 ? nova_karta : 0;
+            }
+            Console.WriteLine(brojac == 31 ? "Pobjedili ste!" : "Izgubili ste!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KartaskaIgra
+ School practice
