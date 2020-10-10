8d15db1dc2a5bb87151e6c88790bd1485cea693c
UltraX <ultraxdn@gmail.com>
2020-10-10 14:27:04 +0200

    Ispravljen input bug
    - ukoliko korisnik ne unese nikakav broj ili se unese neki znak koji nije broj, ispisuje se poruka da se unese broj.

--- KartaskaIgra/Program.cs
+++ KartaskaIgra/Program.cs
@@ -9,11 +9,20 @@ namespace KartaskaIgra
             float brojac = 0;
             while (brojac < 31)
             {
-                Console.WriteLine("Unesite vrijednost karte: ");
-                float nova_karta = Convert.ToInt32(Console.ReadLine());
-                brojac += nova_karta != 0 ? nova_karta : 0;
+                try
+                {
+                    Console.Write("Unesite vrijednost karte: ");
+                    float nova_karta = Convert.ToInt32(Console.ReadLine());
+                    brojac += nova_karta;
+                }
+                catch
+                {
+                    Console.WriteLine("Unesite broj!");
+                } 
+                
             }
             Console.WriteLine(brojac == 31 ? "Pobjedili ste!" : "Izgubili ste!");
+            Console.ReadKey();
         }
     }
 }
